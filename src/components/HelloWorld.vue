7f1ee5ce539dd527fc790093b8cf271e47ae8e09
unknown <vahid.firoozian@gmail.com>
2023-01-25 05:57:20 +0330

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <button type="button" style='visibility:hidden'>Click Me!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+ 
+  mounted() {
+    setTimeout(() => {
+            this.$el.querySelector("button").style.visibility = 'visible';
+        }, 1500);
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.visible {
+  visibility : hidden;
+  }
+</style>
